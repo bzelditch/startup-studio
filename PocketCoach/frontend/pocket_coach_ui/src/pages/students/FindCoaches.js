d54de6c830ade33c1e3e7b867c273e79884ff1e2
anirban21 <anirban@shophop.co.in>
2019-04-04 14:56:55 -0400

    find coaches changes

--- PocketCoach/frontend/pocket_coach_ui/src/pages/students/FindCoaches.js
+++ PocketCoach/frontend/pocket_coach_ui/src/pages/students/FindCoaches.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import FindCoachStepper from  './FindCoachStepper.js'
+import FindCoachCard from  './FindCoachCard.js'
 
 
 export default props => (
   <Typography variant="h1">
-    Find Coaches
-
+    <FindCoachStepper/>
+    <FindCoachCard/>
   </Typography>
 );
