1d42602aeec0879985d0f3d2cdc56f7d7517e40a
bzelditch <bz87@cornell.edu>
2019-04-04 13:40:51 -0400

    fixed conflicts?

--- PocketCoach/frontend/pocket_coach_ui/src/components/index.js
+++ PocketCoach/frontend/pocket_coach_ui/src/components/index.js
@@ -4,7 +4,6 @@ import StudentProfileTabs from './layout/StudentProfileTabs';
 import CommentCard from './videos/CommentCard';
 import FeedbackCard from './videos/FeedbackCard';
 import AnnotationForm from './videos/AnnotationForm';
-<<<<<<< HEAD
 import PlayerControllerExample from './videos/PlayerControllerExample'
 import PlayerAnnotationController from './videos/PlayerAnnotationController'
 import CreateAnnotationDialog from './videos/CreateAnnotationDialog'
@@ -13,14 +12,9 @@ import PlaylistPanel from './videos/PlaylistPanel'
 import RecommendedCoachesPanel from './videos/RecommendedCoachesPanel'
 import StudentProfileLeftPane from './students/StudentProfileLeftPane';
 import StudentProfileRightPane from './students/StudentProfileRightPane';
-=======
 import CompleteFeedbackForm from './videos/CompleteFeedbackForm';
-import PlayerAnnotationController from './videos/PlayerAnnotationController';
-import CreateAnnotationDialog from './videos/CreateAnnotationDialog';
 import CompleteFeedbackDialog from './videos/CompleteFeedbackDialog';
-import CommentsTimeline from './videos/CommentsTimeline';
 import React from "react";
->>>>>>> 2537914301d699e397a66a368610473c8ffa2a9f
 
 export {
   Header,
@@ -34,11 +28,8 @@ export {
   CreateAnnotationDialog,
   CompleteFeedbackDialog,
   CommentsTimeline,
-<<<<<<< HEAD
   PlaylistPanel,
   RecommendedCoachesPanel,
   StudentProfileLeftPane,
   StudentProfileRightPane
-=======
->>>>>>> 2537914301d699e397a66a368610473c8ffa2a9f
 };
