a1d3b2727a72d9dbc6a0fe58f64d2f93350b7b5a
bzelditch <bz87@cornell.edu>
2019-04-04 15:35:28 -0400

    create new project boilerplate

---
+++ PocketCoach/frontend/pocket_coach_ui/src/components/students/UploadVideo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/* Component for users to upload videos when creating a project   */
+
+class UploadVideo extends React.Component {
+    render() {
+        return ();
+    }
+}
+
+
+export default UploadVideo;
